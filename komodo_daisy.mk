0063479949f815368f8cfc0105bd19f51843cc84
ganomin <szymon.wirkus000@gmail.com>
2020-05-22 21:47:56 +0200

    daisy: Dragon-ify!

--- komodo_daisy.mk
+++ komodo_daisy.mk
@@ -5,23 +5,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common EvolutionX stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Komodo OS stuff.
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
 
-# Other EvolutionX releated stuff
-CUSTOM_BUILD_TYPE := UNOFFICIAL
+# Other Komodo OS releated stuff
+KOMODO_OFFICIAL=false
 TARGET_BOOT_ANIMATION_RES := 1080
-#TARGET_INCLUDE_WIFI_EXT := true
 
 # GApps stuff
-#TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_GAPPS_ARCH := arm64
-#TARGET_SUPPORTS_GOOGLE_RECORDER := true
+CURRENT_BUILD_TYPE := gapps
 
 # Maintainer stuff
-EVO_DONATE_URL := https://paypal.me/ganomin
-EVO_MAINTAINER := ganomin🇵🇱
-EVO_SUPPORT_URL := https://t.me/EvolutionXDaisy
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.komodo.maintainer=ganomin🇵🇱
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
